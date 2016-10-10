858f3f22e73da9616d3af919d74b775d9207e582
yaoxin <charlesyaoxin@163.com>
2016-10-10 09:38:05 +0800

    Initial Commit

---
+++ ImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by Charles on 16/10/10.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImagePicker
+//
+//  Created by Charles on 16/10/10.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
